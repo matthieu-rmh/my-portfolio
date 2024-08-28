492e037658a44a4fccb486a548fafb7365d6d634
matthieu-rmh <razafintsalama.rmh@gmail.com>
2024-08-28 16:53:57 +0200

    Init

---
+++ app/components/layout-header.tsx
@@ -0,0 +1,16 @@
+const LayoutHeader = () => {
+    return(
+        <nav className="flex p-5">
+          <div>LOGO</div>
+          <div>
+            <a href="#">Projects</a>
+            <a href="#">About me</a>
+            <a href="#">Contact</a>
+          </div>
+          <div>Buttons</div>
+          <div></div>
+        </nav>
+    );
+}
+
+export default LayoutHeader;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// List of all fonts used throughout the app
+
+import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const outfit = Outfit({ weight: "300", subsets: ["latin"] });
+
